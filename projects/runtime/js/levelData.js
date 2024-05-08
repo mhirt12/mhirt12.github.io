acd99b82226fb2981da9de6e6c075d18880ea069
miles hirt <m.hirt@ardencahillacademy.com>
2024-05-08 13:50:06 +0000

    install projects

--- projects/runtime/js/levelData.js
+++ projects/runtime/js/levelData.js
@@ -14,11 +14,11 @@ var makeLevelData = function (window) {
       {
         name: "Robot Romp",
         number: 1,
-        speed: 3,
+        speed: -3,
         gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
+          { type: "sawblade", x: 400, y: groundY -25},
+          { type: "sawblade", x: 600, y: groundY -120},
+          { type: "sawblade", x: 900, y: groundY -31},
           { type: "reward", "x": 1902, "y": groundY - 60},
           { type: "reward", "x": 1780, "y": groundY - 60},
           { type: "reward", "x": 1150, "y": groundY - 60},
@@ -31,16 +31,16 @@ var makeLevelData = function (window) {
       {
         name: "Robot Rampage",
         number: 2,
-        speed: 2,
+        speed: -3,
         gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
+          { type: "sawblade", x: 400, y: groundY -30},
+          { type: "sawblade", x: 600, y: groundY -31},
+          { type: "sawblade", x: 900, y: groundY -30},
           { type: "reward", "x": 2000, "y": groundY - 60},
           { type: "reward", "x": 2000, "y": groundY - 60},
-          {type: "enemy", x: 1481, y: groundY -65},
-          {type: "enemy", x:1100, y: groundY -53},
-          {type: "enemy", x:1809, y: groundY -70},
+          {type: "enemy", x: 1481, y: groundY -55},
+          {type: "enemy", x:1100, y: groundY -51},
+          {type: "enemy", x:1809, y: groundY -50},
 
 
         ],
@@ -48,19 +48,15 @@ var makeLevelData = function (window) {
       {
         name: "Mordor",
         number: 3,
-        speed: 3,
+        speed: -3,
         gameItems: [
           { type: "sawblade", x: 600, y: groundY},
-          { type: "sawblade", x: 1100, y: groundY},
-          { type: "sawblade", x: 400, y: groundY},
           { type: "sawblade", x: 900, y: groundY},
-          { type: "sawblade", x: 300, y: groundY},
-          { type: "sawblade", x: 1100, y: groundY},
 
-          {type: "enemy", x: 1106, y: groundY -50},
-          {type: "enemy", x:2000, y: groundY}-50,
-          {type: "enemy", x:1699, y: groundY -50},
-          {type: "enemy", x:1440, y: groundY -50},
+          {type: "enemy", x: 1106, y: groundY -115},
+          {type: "enemy", x:2000, y: groundY-51},
+          {type: "enemy", x:1699, y: groundY -15},
+          {type: "enemy", x:1440, y: groundY -54},
 
           { type: "reward", "x": 2000, "y": groundY - 60},
           { type: "reward", "x": 2000, "y": groundY - 60},
